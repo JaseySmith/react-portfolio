26aa9a57119f030c81aa075e39b5806bd8a527e5
JaseySmith <jaseyhsmith@gmail.com>
2023-12-19 23:37:16 -0800

    Added Netlify form handling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,8 @@
 import React from 'react';
-import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComments } from '@fortawesome/free-regular-svg-icons';
 
 function Contact() {
-  const [status, setStatus] = useState("Send Message");
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setStatus("Sending...");
-    const { name, email, message } = e.target.elements;
-    let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
-    });
-    setStatus("Send Message");
-    let result = await response.json();
-    alert(result.status);
-  };
 
   return (
     <div>
@@ -32,11 +10,12 @@ function Contact() {
             <FontAwesomeIcon icon={faComments} />
             <h2>Say Hello!</h2>
             <p className="text mb-40">Ready to collaborate or have any questions? Feel free to reach out anytime. <br></br>Excited to create with you.</p>
-            <form onSubmit={handleSubmit}>
+            <form netlify>
                 <input type="text" id="name" name="name" placeholder="Name" pattern="[A-Za-z]+" title="Only letters are allowed" required></input>
                 <input type="email" id="email" name="email" placeholder="Enter email" required></input>
                 <textarea name="message" id="message" placeholder="How may I help?"></textarea>
-                <button className="btn" type="submit" name="submit" value="Send Message">{status}</button>
+                <button className="btn" type="submit" name="submit" value="Send Message">Send Message</button>
+                <input type="hidden" name="form-name" value="contact"></input>
             </form>
         </div>
     </div>
