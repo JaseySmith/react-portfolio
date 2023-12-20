e4cb85e973a0d78d1d261de975f74650208ed764
JaseySmith <jaseyhsmith@gmail.com>
2023-12-20 15:28:58 -0800

    Updated contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,13 +4,28 @@ import { faComments } from '@fortawesome/free-regular-svg-icons';
 
 function Contact() {
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+    
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => alert("Thank you for your submission"))
+      .catch((error) => alert(error));
+  };
+  
   return (
     <div>
         <div id="contact">
             <FontAwesomeIcon icon={faComments} />
             <h2>Say Hello!</h2>
             <p className="text mb-40">Ready to collaborate or have any questions? Feel free to reach out anytime. <br></br>Excited to create with you.</p>
-            <form name="contact" method="post">
+            <form name="contact" method="post" onSubmit={handleSubmit}>
                 <input type="hidden" name="form-name" value="contact"></input>
                 <input type="text" id="name" name="name" placeholder="Name" pattern="[A-Za-z]+" title="Only letters are allowed" required></input>
                 <input type="email" id="email" name="email" placeholder="Enter email" required></input>
