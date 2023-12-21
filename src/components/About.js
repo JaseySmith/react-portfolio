9a79d9e5f1747e260fd2c6abc747bfd55cc1a00e
JaseySmith <jaseyhsmith@gmail.com>
2023-12-20 22:59:58 -0800

    Updated about section

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ function About() {
         <div id="about">
             <div className="content">
               <h1>Hi, I'm Jasey.</h1>
-              <p className="text"><span>Front-end web developer</span> located in California, skilled in MERN, CSS, JavaScript. Expert in WordPress and Shopify. Crafting <span>exceptional digital experiences</span> through <a href="https://www.freecodecamp.org/">freecodecamp.org</a> certifications.<br></br><br></br><div className="hide-on-mobile">Diving beyond code, I venture into hiking trails, golf courses, and Sierra Nevada slopes, weaving tech and nature to craft dynamic web solutions that surpass expectations.</div></p>
+              <p className="text"><span>Front-end web developer</span> located in California, skilled in MERN, CSS, JavaScript. Expert in WordPress and Shopify. Crafting <span>exceptional digital experiences</span> through <a href="https://www.freecodecamp.org/">freecodecamp.org</a> certifications.<br></br><br></br><div className="hide-on-mobile">Away from computer screens, I explore forest trails, tee up on sunny golf courses, and shred nearby slopes, combining tech skills with a passion for the outdoors to create standout web solutions.</div></p>
               <a className="btn" href="#projects">View Projects</a>
               <Socials />
             </div>
