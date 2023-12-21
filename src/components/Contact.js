7563bcd72cf8983328b9311e31689206fa244dc7
JaseySmith <jaseyhsmith@gmail.com>
2023-12-20 16:19:06 -0800

    Updated contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComments } from '@fortawesome/free-regular-svg-icons';
 
 function Contact() {
+  const [buttonText, setButtonText] = useState('Send Message');
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
   
     const myForm = event.target;
     const formData = new FormData(myForm);
+
+    setButtonText('Sending...');
     
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => alert("Thank you for your submission"))
-      .catch((error) => alert(error));
+    try {
+      await fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString(),
+      });
+
+      setButtonText('Message Sent');
+      myForm.reset();
+    } catch (error) {
+      setButtonText('Send Message');
+      alert(`Error: ${error}`);
+    }
   };
   
   return (
@@ -27,10 +36,10 @@ function Contact() {
             <p className="text mb-40">Ready to collaborate or have any questions? Feel free to reach out anytime. <br></br>Excited to create with you.</p>
             <form name="contact" method="post" onSubmit={handleSubmit}>
                 <input type="hidden" name="form-name" value="contact"></input>
-                <input type="text" id="name" name="name" placeholder="Name" pattern="[A-Za-z]+" title="Only letters are allowed" required></input>
+                <input type="text" id="name" name="name" placeholder="Name" pattern="[A-Za-z ]+" title="Only letters are allowed" required></input>
                 <input type="email" id="email" name="email" placeholder="Enter email" required></input>
                 <textarea name="message" id="message" placeholder="How may I help?"></textarea>
-                <button className="btn" type="submit" name="submit" value="Send Message">Send Message</button>
+                <button className="btn" type="submit" name="submit" value="Send Message">{buttonText}</button>
             </form>
         </div>
     </div>
