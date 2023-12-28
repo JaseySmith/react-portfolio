fd777e5daaa2766b8e1146e22a8e372c404b0eeb
Jasey Smith <jaseyhsmith@gmail.com>
2023-12-28 08:17:06 -0800

    Updated About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Background from './Background';
-import Socials from './Socials';
 
 function About() {
   return (
@@ -8,9 +7,11 @@ function About() {
         <div id="about">
             <div className="content">
               <h1>Hi, I'm Jasey.</h1>
-              <p className="text"><span>Front-end web developer</span> located in California, skilled in MERN, CSS, JavaScript. Expert in WordPress and Shopify. Crafting <span>exceptional digital experiences</span> through <a href="https://www.freecodecamp.org/">freecodecamp.org</a> certifications.<br></br><br></br><div className="hide-on-mobile">Away from computer screens, I explore forest trails, tee up on sunny golf courses, and shred nearby slopes, combining tech skills with a passion for the outdoors to create standout web solutions.</div></p>
-              <a className="btn" href="#projects">View Projects</a>
-              <Socials />
+              <p className="text"><span>Front-end web developer</span> located in California, skilled in MERN, CSS, JavaScript. Expert in WordPress and Shopify. Crafting <span>exceptional digital experiences</span> through <a href="https://www.freecodecamp.org/" target="_blank">freecodecamp.org</a> certifications.<div className="hide-on-mobile"><br></br>Away from computer screens, I explore forest trails, tee up on sunny golf courses, and shred nearby slopes, combining tech skills with a passion for the outdoors to create standout web solutions.</div></p>
+              <div className="flex">
+                <a className="btn" href="#projects">View Projects</a>
+                <a className="alt-btn" href="#skills"><span>&lt;</span>View Skills<span>/&gt;</span></a>
+              </div>
             </div>
             <Background />
         </div>
