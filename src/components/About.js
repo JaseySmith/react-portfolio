f2f6629faca6c52a428381ec0b106a8c1835b558
Jasey Smith <jaseyhsmith@gmail.com>
2024-08-28 22:18:09 -0700

    Update UI Design

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ function About() {
             <FontAwesomeIcon icon={faUser} />
             <h2>About Me</h2>
             <div className="flex">
-              <p className="text">Hello, I'm Jasey, a full-stack web developer with a passion for creating impactful digital experiences. My journey into the world of coding began four years ago when I discovered the endless possibilities it offered. This passion drove me to start my formal education through <a href="https://www.freecodecamp.org/" target="_blank" rel="noreferrer">freeCodeCamp.org.</a><br></br><br></br>
+              <p className="text">Hi, I'm Jasey, a full-stack web developer. My journey into the world of coding began four years ago when I discovered the endless possibilities it offered. This discovery drove me to start my formal education through <a href="https://www.freecodecamp.org/" target="_blank" rel="noreferrer">freeCodeCamp.org.</a><br></br><br></br>
 
 Over the years, I've earned multiple certifications in a dozen programming languages and have had the pleasure of helping clients enhance their online presence. I specialize in the React JavaScript library and work primarily with the M.E.R.N. stack. If you have any questions or would like to discuss a project, feel free to reach out at <a href="mailto:jaseyhsmith@gmail.com" target="_blank" rel="noreferrer">jaseyhsmith@gmail.com.</a></p>
               <div className="container">
